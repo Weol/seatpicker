32b08b9e21ae1cfa8f96989bd371ef76f845bae6
Erik Nystø Rahka <erik.rahka@kredinor.no>
2023-02-08 14:24:53 +0100

    asd

--- Infrastructure/Adapters/Database/SeatpickerContext.cs
+++ Infrastructure/Adapters/Database/SeatpickerContext.cs
@@ -1,24 +1,48 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Seatpicker.Infrastructure.Adapters.Database;
 
 public class SeatpickerContext : DbContext
 {
-    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<ReservationDao> Reservations { get; set; }
 
-    public DbSet<Seat> Seat { get; set;  }
+    public DbSet<SeatDao> Seat { get; set;  }
     
-    public DbSet<Floorplan> Floorplans { get; set;  }
+    public DbSet<FloorplanDao> Floorplans { get; set;  }
 }
 
-public class Reservation
+public class ReservationDao
 {
+    [Key]
+    public int ReservationId { get; set; }
+
+    [ForeignKey(nameof(Seat))]
+    public int SeatId { get; set; }
+    public SeatDao Seat;
+    
+    public string Nick { get; set; }
+    
+    public string DiscordId { get; set; }
+    
+    public string DiscordAvatar { get; set; }
 }
 
-public class Floorplan 
+public class FloorplanDao 
 {
+    [Key]
+    public int FloorplanId { get; set; }
+
+    public byte[] Backdrop { get; set; }
 }
 
-public class Seat
+public class SeatDao
 {
+    [Key]
+    public int SeatId { get; set; }
+
+    [ForeignKey(nameof(Floorplan))]
+    public int FloorplanId { get; set; }
+    public FloorplanDao Floorplan;
 }
