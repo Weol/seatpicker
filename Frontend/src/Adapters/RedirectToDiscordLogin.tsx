8e327b3fb24fcd430217fe555c7f586fce2f4370
Erik Nystø Rahka <erikrahka@gmail.com>
2023-03-26 23:36:21 +0200

    asd

--- Frontend/src/Adapters/RedirectToDiscordLogin.tsx
+++ Frontend/src/Adapters/RedirectToDiscordLogin.tsx
@@ -1,5 +1,7 @@
 import config from "../config"
 
 export default function RedirectToDiscordLogin() {
-  window.location.replace(config.DiscordAuthorizationUrl)
+  var redirectUrl = window.location.origin + "/redirect-login"
+  var uri = `https://discord.com/api/oauth2/authorize?client_id=1042448593312821248&redirect_uri=${encodeURIComponent(redirectUrl)}&response_type=code&scope=identify`
+  window.location.replace(uri)
 }
