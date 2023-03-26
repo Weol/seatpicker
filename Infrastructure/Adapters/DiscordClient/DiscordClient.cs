69483e1af17e5226c6df09d6a1ec69fbc43136c8
Erik Nystø Rahka <erikrahka@gmail.com>
2023-03-26 23:38:57 +0200

    uff 2

--- Infrastructure/Adapters/DiscordClient/DiscordClient.cs
+++ Infrastructure/Adapters/DiscordClient/DiscordClient.cs
@@ -1,102 +1,29 @@
-﻿using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using Discord;
+using Discord.Rest;
 using Microsoft.Extensions.Options;
 using Seatpicker.Application.Features.Login;
-using Seatpicker.Application.Features.Login.Ports;
+using Seatpicker.Application.Features.Reservation.EventHandlers;
+using Seatpicker.Domain;
 
-namespace Seatpicker.Infrastructure.Adapters.DiscordClient;
+namespace Seatpicker.Infrastructure.Adapters;
 
-internal class DiscordClient : IDiscordAccessTokenProvider, IDiscordLookupUser
+public class DiscordClient : IInviteDiscordUser
 {
-    private readonly HttpClient httpClient;
-    private readonly JsonSerializerOptions jsonSerializerOptions;
-    private readonly DiscordClientOptions options;
-    private readonly ILogger<DiscordClient> logger;
+    private readonly DiscordRestClient discordRestClient;
 
-    public DiscordClient(HttpClient httpClient, IOptions<DiscordClientOptions> options,
-        JsonSerializerOptions jsonSerializerOptions, ILogger<DiscordClient> logger)
+    public DiscordClient(IOptions<DiscordClientOptions> options)
     {
-        this.httpClient = httpClient;
-        this.jsonSerializerOptions = jsonSerializerOptions;
-        this.options = options.Value;
-        this.logger = logger;
+        var client = new DiscordRestClient();
+
+        client.LoginAsync(TokenType.Bot, options.Value.Token)
+            .GetAwaiter()
+            .GetResult();
+
+        discordRestClient = client;
     }
 
-    public async Task<string> GetFor(string discordToken)
+    public async Task Invite(User user)
     {
-        var response = await httpClient.PostAsync("oauth2/token", new FormUrlEncodedContent(new[]
-        {
-            new KeyValuePair<string, string>("grant_type", "authorization_code"),
-            new KeyValuePair<string, string>("client_id", options.ClientId),
-            new KeyValuePair<string, string>("client_secret", options.ClientSecret),
-            new KeyValuePair<string, string>("redirect_uri", options.RedirectUri.ToString()),
-            new KeyValuePair<string, string>("code", discordToken),
-        }));
-
-        var dto = await DeserializeContent<DiscordAccessTokenDto>(response);
-        return dto.AccessToken;
-    }
-
-    public async Task<DiscordUser> Lookup(string accessToken)
-    {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, "users/@me");
-
-        requestMessage.Headers.Authorization =
-            new AuthenticationHeaderValue("Bearer", accessToken);
-
-        var response = await httpClient.SendAsync(requestMessage);
-        return await DeserializeContent<DiscordUser>(response);
-    }
-
-    private async Task<TModel> DeserializeContent<TModel>(HttpResponseMessage response)
-    {
-        var body = await response.Content.ReadAsStringAsync();
-        if (response.IsSuccessStatusCode)
-        {
-            logger.LogInformation("Discord API responded with code {StatusCode} and body {body}", response.StatusCode, body);
-            
-            return JsonSerializer.Deserialize<TModel>(body, jsonSerializerOptions)
-                   ?? throw new NullReferenceException($"Could not deserialize Discord response to {nameof(TModel)}");
-        }
-
-        logger.LogError("Non-successful response code from Discord {@ResponseInfo}", new
-        {
-            response.StatusCode,
-            Body = body,
-        });
-
-        throw new DiscordException($"Non-successful response code from Discord {response.StatusCode}");
-    }
-
-    private class DiscordAccessTokenDto
-    {
-        [JsonPropertyName("access_token")]
-        public string AccessToken { get; set; } = null!;
-
-        [JsonPropertyName("expires_in")]
-        public int ExpiresIn { get; set; }
-        
-        [JsonPropertyName("refresh_token")]
-        public string RefreshToken { get; set; } = null!;
-
-        [JsonPropertyName("scopes")]
-        public IEnumerable<string> Scopes { get; set; } = null!;
-
-        [JsonPropertyName("token_type")]
-        public string TokenType { get; set; } = null!;
-    }
-}
-
-internal class DiscordException : Exception
-{
-    public Exception? Exception { get; set; }
-
-    public DiscordException(string message, Exception innerException) : base(message, innerException)
-    {
-    }
-
-    public DiscordException(string message) : base(message)
-    {
+        var guild = await discordRestClient.GetGuildsAsync();
     }
 }
