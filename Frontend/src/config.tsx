efdb0abb7085c34452a4be4f5da8dfe53083d267
Erik Nystø Rahka <erikrahka@gmail.com>
2023-03-26 21:32:16 +0200

    asdasd

--- Frontend/src/config.tsx
+++ Frontend/src/config.tsx
@@ -5,7 +5,7 @@ type Config = {
 }
 
 let config: Config = {
-  ApiBaseUrl: "",
+  ApiBaseUrl: "api",
   DiscordAuthorizationUrl: "https://discord.com/api/oauth2/authorize?client_id=1042448593312821248&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect-login&response_type=code&scope=identify",
   DiscordAvatarBaseUrl: "https://cdn.discordapp.com/avatars/"
 }
