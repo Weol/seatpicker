5d9bef4e1e11a3956dedf60434a27da0e38df1f0
Erik Nystø Rahka <erik.rahka@kredinor.no>
2023-03-27 13:39:00 +0200

    feat: asd

--- Infrastructure/Adapters/DiscordClient/DiscordClient.cs
+++ Infrastructure/Adapters/DiscordClient/DiscordClient.cs
@@ -1,7 +1,6 @@
 ﻿using Discord;
 using Discord.Rest;
 using Microsoft.Extensions.Options;
-using Seatpicker.Application.Features.Login;
 using Seatpicker.Application.Features.Reservation.EventHandlers;
 using Seatpicker.Domain;
 
@@ -10,20 +9,32 @@ namespace Seatpicker.Infrastructure.Adapters;
 public class DiscordClient : IInviteDiscordUser
 {
     private readonly DiscordRestClient discordRestClient;
+    private readonly string guildId;
 
     public DiscordClient(IOptions<DiscordClientOptions> options)
     {
-        var client = new DiscordRestClient();
+        discordRestClient = CreateDiscordRestClient(options.Value.Token);
+        guildId = options.Value.GuildId;
+    }
 
-        client.LoginAsync(TokenType.Bot, options.Value.Token)
-            .GetAwaiter()
-            .GetResult();
-
-        discordRestClient = client;
+    private async Task<RestGuild> GetGuild()
+    {
+        return await discordRestClient.GetGuildAsync(ulong.Parse(guildId));
     }
 
     public async Task Invite(User user)
     {
-        var guild = await discordRestClient.GetGuildsAsync();
+        var guild = GetGuild();
+    }
+
+    private static DiscordRestClient CreateDiscordRestClient(string token)
+    {
+        var client = new DiscordRestClient();
+
+        client.LoginAsync(TokenType.Bot, token)
+            .GetAwaiter()
+            .GetResult();
+
+        return client;
     }
 }
