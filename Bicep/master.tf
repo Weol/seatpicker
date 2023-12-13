29248b6950ca9d56194c61d20786fe4f917f7ce7
Erik Nystø Rahka <erikrahka@gmail.com>
2023-12-13 23:51:47 +0100

    wip

---
+++ Bicep/master.tf
@@ -0,0 +1,50 @@
+variable "azure_tenant_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_subscription_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_client_secret" {
+  type = string
+  sensitive = true
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  cloud {
+    organization = "Saltenlan"
+
+    workspaces {
+      name = "saltenlan"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+
+  subscription_id = var.azure_subscription_id
+  tenant_id = var.azure_tenant_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+}
+
+resource "azurerm_resource_group" "resourceGroup" {
+  name     = "testy"
+  location = "East Norway"
+}
